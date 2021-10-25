a5a72cfe426fdc731ceff647d1fb1b78771b684b
Alexey Kapanov <alesnim@gmail.com>
2021-10-25 10:30:00 +0300

    Update README.md and rework Counter.java

--- src/com/company/Counter.java
+++ src/com/company/Counter.java
@@ -1,27 +1,63 @@
 package com.company;
 
-public class Counter {
-    private int count = 0;
 
+/**
+ * Класс-счетчик поддерживающий разные варианты подсчета
+ */
+public class Counter {
+    /**
+     * подсчет по инстансам
+     **/
+    private int count = 0;
+    /**
+     * общий счетчик
+     **/
     private static int globalCount = 0;
 
+    /**
+     * Инкремент счетчика
+     */
     public void adding() {
         count++;
     }
 
+    /**
+     * Инкремент на заданную величину
+     *
+     * @param i сколько добавить
+     */
     public void customAdding(int i) {
         count += i;
     }
 
+    /**
+     * Инкремент глобального счетчика
+     */
+    public static void globalAdding() {
+        globalCount++;
+    }
+
+
+    /**
+     * Узнать значение счетчика
+     *
+     * @return число в счетчике
+     */
     public int getCount() {
         return count;
     }
 
+    /**
+     * Узнать значение глобального счетчика
+     *
+     * @return число в глобальном счетчике
+     */
     public static int getGlobalCount() {
         return globalCount;
     }
 
-    public static void globalAdding() {
-        globalCount++;
-    }
+
+    // TODO: Реализовать метод выводящий счетчики трех объектов Counter
+
+
 }
